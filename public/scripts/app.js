c2a081507915d45255461eafc508a2de33fea5d3
vagrant <noura4403@gmail.com>
2022-01-09 22:54:44 +0000

    working on GET req for posts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,16 @@
 // Client facing scripts here
+const getPosts = () => {
+  return $.ajax({
+    url: 'http://localhost:8080/posts',
+    method: 'GET'
+  })
+}
+$(document).ready(() => {
+  let posts;
+  getPosts().done((data) => {
+    posts = data;
+  })
+
+
+
+});
