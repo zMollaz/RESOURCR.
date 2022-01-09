ae25dd0ed98bd11c933409340f5256a71135965e
vagrant <noura4403@gmail.com>
2022-01-09 23:42:09 +0000

    rendering images from GET

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,35 @@ const getPosts = () => {
     method: 'GET'
   })
 }
+const createPostElement = (post) => {
+  console.log("post", post);
+  let $post = `
+  <div class="card">
+    <img src="${post.img_src}">
+  </div>
+
+
+
+  `
+  return $post;
+}
+
+const renderPosts = (posts) => {
+  console.log("posts",posts);
+  const $postContainer = $('.post-container');
+  for (const post of posts) {
+  const $post = createPostElement(post);
+  $postContainer.prepend($post);
+  }
+}
+
+
+
 $(document).ready(() => {
   let posts;
   getPosts().done((data) => {
-    posts = data;
+  posts = data.posts;
+  renderPosts(posts);
   })
 
 
