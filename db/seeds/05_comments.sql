428fd5b013eb4f3d23fdeb4259890fd0e7f7a7dc
vagrant <noura4403@gmail.com>
2022-01-14 01:15:51 +0000

    new db seed

--- db/seeds/05_comments.sql
+++ db/seeds/05_comments.sql
@@ -1,7 +1,14 @@
 INSERT INTO comments (user_id, post_id, comment)
 VALUES (1,1,'New year new me, finally going to start to learn coding, thanks!'),
-(1,2,'Will def check these out, so behind on my watch list'),
-(1,3,'Looks so good! We all need a bit of comfort food right now'),
-(2,4,'Always wanted to learn front-end dev, will take a look'),
-(2,5,'I used to love watching TCM'),
-(2,6,'Need to start eating more healthy, thanks for the reminder!');
+(1,2,'Will check this out, thanks!'),
+(1,3,'Will def check these out, so behind on my watch list'),
+(1,4,'Damn I’d like to visit one of these theatres, so beautiful'),
+(1,5,'So Hungry rn, thanks guys lol.'),
+(1,6,'Looks so good! We all need a bit of comfort food right now'),
+(2,7,'Always wanted to learn front-end dev, will take a look'),
+(2,8,'Will show this to my niece, thanks!'),
+(2,9,'I used to love watching TCM'),
+(2,10,'Didn’t know the ins and outs of a movie set, fun to learn.'),
+(2,11,'Need to start eating more healthy, thanks for the reminder!'),
+(2,12,'Wow didn’t know about some of these places…awesome');
+
